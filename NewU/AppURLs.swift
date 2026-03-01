e4261fcf66b8a73f518b25c4067bc313de024add
Chad Newbry <chad.newbry@gmail.com>
2026-02-28 19:44:41 -0500

    feat: centralize app URLs into AppURLs constants

---
+++ NewU/AppURLs.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/// Central location for all external app URLs.
+enum AppURLs {
+    static let privacyPolicy = URL(string: "https://chadnewbry.github.io/NewU/privacy.html")!
+    static let termsOfService = URL(string: "https://chadnewbry.github.io/NewU/terms.html")!
+    static let support = URL(string: "https://chadnewbry.github.io/NewU/support.html")!
+    static let contactEmail = URL(string: "mailto:chad.newbry@gmail.com")!
+    static let appStore = URL(string: "https://apps.apple.com/app/newu")!
+}
